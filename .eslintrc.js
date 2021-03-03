8556606580250e3ab6534a4913d9dc0d8814abc4
NoDocCat <nodoccat@outlook.com>
2021-03-03 22:48:25 +0800

    build(deps-dev): 更新依赖 (#2, #3, #4, #5, #6)
    bump @typescript-eslint/eslint-plugin from 4.14.2 to 4.16.1 (close #5)
    bump @typescript-eslint/parser from 4.14.2 to 4.16.1 (close #6)
    bump eslint-config-prettier from 7.2.0 to 8.1.0 (close #2)
    bump eslint from 7.19.0 to 7.21.0 (close #3)
    bump husky from 5.0.9 to 5.1.3
    bump lint-staged from 10.5.3 to 10.5.4
    bump pinst from 2.1.4 to 2.1.6
    bump ts-jest from 26.5.0 to 26.5.2 (close #4)
    bump typescript from 4.1.3 to 4.2.2
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: NoDocCat <nodoccat@outlook.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "prettier/@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
