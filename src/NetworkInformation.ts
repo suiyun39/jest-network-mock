fff8585e2a63d609f53bcc0832c21f882b3ae350
NoDocCat <nodoccat@outlook.com>
2023-02-23 22:39:48 +0800

    feat: 完成事件核心

--- src/NetworkInformation.ts
+++ src/NetworkInformation.ts
@@ -1,5 +1,7 @@
-export type EffectiveConnectionType = 'slow-2g' | '2g' | '3g' | '4g'
-export type ConnectionType = 'bluetooth' | 'cellular' | 'ethernet' | 'none' | 'wifi' | 'wimax' | 'other' | 'unknown'
+import { PRESET_4G } from './preset'
+
+type EffectiveConnectionType = 'slow-2g' | '2g' | '3g' | '4g'
+type ConnectionType = 'bluetooth' | 'cellular' | 'ethernet' | 'none' | 'wifi' | 'wimax' | 'other' | 'unknown'
 
 export interface ConnectionInfo {
   downlink: number
@@ -8,4 +10,22 @@ export interface ConnectionInfo {
   rtt: number
   saveData: boolean
   type: ConnectionType
+}
+
+export class NetworkInformation extends EventTarget implements ConnectionInfo {
+  downlink = PRESET_4G.downlink
+  downlinkMax = PRESET_4G.downlinkMax
+  effectiveType = PRESET_4G.effectiveType
+  rtt = PRESET_4G.rtt
+  saveData = PRESET_4G.saveData
+  type = PRESET_4G.type
+
+  onchange: EventListener | null = null
+
+  dispatchEvent (event: Event) {
+    if (event.type === 'change') {
+      this.onchange?.({ ...event, type: 'change', currentTarget: this, target: this })
+    }
+    return super.dispatchEvent(event)
+  }
 }
